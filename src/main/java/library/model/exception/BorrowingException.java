a6f24febe2567c8c523774805633597b797fba52
Denis Baltor <dbaltor@pivotal.io>
2020-05-28 19:09:39 +0100

    Added capabilities of borrowing/returning books

---
+++ src/main/java/library/model/exception/BorrowingException.java
@@ -0,0 +1,16 @@
+package library.model.exception;
+
+import java.util.Set;
+
+import library.model.BookService.BorrowingErrors;
+
+public class BorrowingException extends Exception {
+    public Set<BorrowingErrors> errors;
+
+    public BorrowingException(Set<BorrowingErrors> errors) {
+        super();
+        this.errors = errors;
+    }
+    
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/resources/public/client.js
@@ -0,0 +1,34 @@
+
+const callAndAlert = async (URL = '', data = {}, reload = false) => {
+    //console.log("URL = " + URL)
+    //console.log("data as JSON = " + JSON.stringify(data))
+    try {
+        const response = await fetch(URL, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const text = await response.text()
+        alert(text)
+        if (reload) {
+            location.reload()
+        }
+    } catch (e) {
+        alert(e.text);
+    }
+}
+
+const borrowBooks = (books) => {
+    const readerId = prompt('Please enter the Reader ID.')
+    if (readerId) {
+        if (isNaN(readerId)) {
+            alert('Reader ID must be a whole number.')
+            return
+        }
+        callAndAlert('/readers/' + readerId + '/borrowBooks', books, true)
+    }
+}
