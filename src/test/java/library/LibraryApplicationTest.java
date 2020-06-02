b6cc439c6aa2a4841e122ec1085304c4e8cdfe9a
Denis Baltor <dbaltor@pivotal.io>
2020-06-02 12:00:52 +0100

    Added tests for borrowing and returning books

--- src/test/java/library/LibraryApplicationTest.java
+++ src/test/java/library/LibraryApplicationTest.java
@@ -1,8 +1,12 @@
 package library;
 
 import library.model.Book;
+import library.model.Reader;
 import library.model.BookRepository;
 import library.model.BookService;
+import library.model.ReaderRepository;
+import library.model.exception.BorrowingException;
+import library.model.exception.ReturningException;
 
 import lombok.val;
 import com.github.javafaker.Faker;
@@ -24,6 +28,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import static java.util.stream.Collectors.*;
+import java.util.Date;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -42,14 +47,20 @@ public class LibraryApplicationTest{
 	private BookRepository bookRepository;
 	@Autowired
 	private BookService bookService;
+	@Autowired
+	private ReaderRepository readerRepository;
 
-	// List of books created during the test to be removed right after
+	// List of books created during the tests to be removed right after
 	private List<Book> testBooks = new ArrayList<>();
+	// List of readers created during the tests to be removed right after
+	private List<Reader> testReaders = new ArrayList<>();
 	private static final int NUM_TEST_BOOKS = 20;
 	
     @After
     public void teardown() {
-		//Delete all books created for the tests
+		// Delete all readers created for the tests
+		readerRepository.deleteAll(testReaders);
+		// Delete all books created for the tests
 		bookRepository.deleteAll(testBooks);
 	}
 
@@ -71,6 +82,7 @@ public class LibraryApplicationTest{
 			Book.of("Java","","",""),
 			Book.of("Node","","",""),
 			Book.of("Go","","",""));
+		// Added to testBooks to be removed durig the teardown
 		testBooks = (List<Book>)bookRepository.saveAll(testBooks);
 		//When
 		Optional<Book> book = bookService.retrieveBook(testBooks.get(1).getId());
@@ -92,6 +104,7 @@ public class LibraryApplicationTest{
 					.map(book -> book.getId())
 					.collect(toList());
 			//When
+			// Added to testBooks to be removed durig the teardown
 			testBooks = bookService.findBooksByIds(ids);
 			//Then
 			assertThat(testBooks.size(), is(NUM_TEST_BOOKS));
@@ -102,8 +115,56 @@ public class LibraryApplicationTest{
 		//Given
 		bookService.loadDatabase(Optional.of(NUM_TEST_BOOKS), Optional.empty());
 		//When
+		// Added to testBooks to be removed durig the teardown
 		testBooks = bookService.retrieveBooks(Optional.empty(), Optional.empty());
 		//Then
 		assertThat(testBooks.size(), is(NUM_TEST_BOOKS));
+	}
+
+	@Test
+	public void shouldBorrowBooks() throws BorrowingException{
+		// Given
+		// Added to testReaders to be removed durig the teardown
+		testReaders.add(readerRepository.save(Reader.of(
+			"John", 
+			"Doe", 
+			new Date(),
+			"",
+			"")));
+		testBooks = List.of(
+			Book.of("Java","","",""),
+			Book.of("Node","","",""),
+			Book.of("Go","","",""));
+		// Added to testBooks to be removed durig the teardown
+		testBooks = (List<Book>)bookRepository.saveAll(testBooks);
+		// When
+		bookService.borrowBooks(testBooks, testReaders.get(0));
+		// Then
+		val borrowedBooks = bookService.findBooksByReaderId(testReaders.get(0).getId());
+		assertThat(borrowedBooks.size(), is(testBooks.size()));
+	}
+
+	@Test
+	public void shouldReturnBooks() throws BorrowingException, ReturningException{
+		// Given
+		// Added to testReaders to be removed durig the teardown
+		testReaders.add(readerRepository.save(Reader.of(
+			"John", 
+			"Doe", 
+			new Date(),
+			"",
+			"")));
+		testBooks = List.of(
+			Book.of("Java","","",""),
+			Book.of("Node","","",""),
+			Book.of("Go","","",""));
+		// Added to testBooks to be removed durig the teardown
+		testBooks = (List<Book>)bookRepository.saveAll(testBooks);
+		bookService.borrowBooks(testBooks, testReaders.get(0));
+		// When
+		bookService.returnBooks(testBooks, testReaders.get(0));
+		// Then
+		val borrowedBooks = bookService.findBooksByReaderId(testReaders.get(0).getId());
+		assertThat(borrowedBooks.size(), is(0));
 	}
 }
