76ac128f573e44d133e408b1f70d74ada33f4a6f
Denis Baltor <dbaltor@pivotal.io>
2020-06-03 20:27:57 +0100

    Reversed databases cleanup order

--- src/main/java/library/LibraryApplication.java
+++ src/main/java/library/LibraryApplication.java
@@ -77,7 +77,7 @@ public class LibraryApplication {
 
     @PreDestroy
     public void onExit() {
-        bookService.cleanUpDatabase();
         readerService.cleanUpDatabase();
+        bookService.cleanUpDatabase();
     }
 }
